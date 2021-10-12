08f40432ea49d45b37bd27e574398aa484dbfe29
gcc-vps <gcc2017@gmail.com>
2021-10-12 18:10:26 +0800

    add dt

--- pipe-alias.R
+++ pipe-alias.R
@@ -2,3 +2,14 @@ extract <- `[`
 add <- `+`
 sub <- `-`
 add_col <- `$<-`
+dt <-  function(x, a =500, b = 200) {
+	DT::datatable(x, extensions = c('Buttons', 'Scroller'), 
+		      options = list(buttons =c('copy', 'csv', 'excel', 'print', 'pdf'),
+				     dom = c('Bfrtip'),
+				     deferRender = TRUE,
+				     scrollY = b,
+				     scrollX = a,
+				     scroller = TRUE
+				     )
+	)
+}
